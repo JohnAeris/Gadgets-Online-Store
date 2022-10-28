4c4b0b029d676681f02c2e1893021f4b06c24cad
johnaeris <barrotjohnaeris@gmail.com>
2022-10-28 21:42:52 +0800

    Add function to buttons

--- app/src/main/java/com/example/gadgetsonlinestore/LoginActivity.kt
+++ app/src/main/java/com/example/gadgetsonlinestore/LoginActivity.kt
@@ -1,11 +1,24 @@
 package com.example.gadgetsonlinestore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.fragment_get_started_screen.view.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        tvRegister.setOnClickListener{
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+
+        tvForgotPassword.setOnClickListener {
+            startActivity(Intent(this, ForgotPasswordAcitivity::class.java))
+        }
     }
 }
