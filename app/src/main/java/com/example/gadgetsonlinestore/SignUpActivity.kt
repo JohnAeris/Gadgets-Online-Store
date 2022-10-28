624718c0d7cc1c815b3ca3b63789c7bff8ab9ebf
johnaeris <barrotjohnaeris@gmail.com>
2022-10-28 21:39:32 +0800

    Add Sign up activity

---
+++ app/src/main/java/com/example/gadgetsonlinestore/SignUpActivity.kt
@@ -0,0 +1,15 @@
+package com.example.gadgetsonlinestore
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+
+    }
+}
