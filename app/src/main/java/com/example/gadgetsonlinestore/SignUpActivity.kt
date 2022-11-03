d0e1160c383a2baac6c5855f3512d105dbbbaae4
johnaeris <barrotjohnaeris@gmail.com>
2022-11-03 23:11:10 +0800

    Add user registration

--- app/src/main/java/com/example/gadgetsonlinestore/SignUpActivity.kt
+++ app/src/main/java/com/example/gadgetsonlinestore/SignUpActivity.kt
@@ -3,13 +3,60 @@ package com.example.gadgetsonlinestore
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private val personCollectionRef = Firebase.firestore.collection("Users")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
+        auth = FirebaseAuth.getInstance()
 
+        tvLoginHere.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        btnRegister.setOnClickListener {
+            register()
+        }
+
+    }
+
+    private fun register() {
+        val firstName = findViewById<EditText>(R.id.etFirstname).text.toString()
+        val lastName = findViewById<EditText>(R.id.etLastname).text.toString()
+        val email = findViewById<EditText>(R.id.etUsername).text.toString()
+        val password = findViewById<EditText>(R.id.etPassword).text.toString()
+
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    auth.createUserWithEmailAndPassword(email, password).await()
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@SignUpActivity, "Successfully Registered", Toast.LENGTH_LONG).show()
+                    }
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@SignUpActivity, e.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+
+        }
     }
 }
