a219b947ae92c5aeb57167220680862659c9c461
johnaeris <barrotjohnaeris@gmail.com>
2022-11-04 23:53:01 +0800

    Modify nothing

---
+++ app/src/main/java/com/example/gadgetsonlinestore/data/User.kt
@@ -0,0 +1,8 @@
+package com.example.gadgetsonlinestore.data
+
+data class User(
+    var firstName: String = "John",
+    var lastName: String = "Doe",
+    var email: String = "",
+    var password: String = ""
+)
